80ce7403ce7b084524e0c4e39d8aadad80d65c9d
xd.faunredd <andrew.furevych@gmail.com>
2024-10-06 19:00:30 +0300

    add fetching contacts to the app component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,22 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 import "./App.css";
 import { selectError, selectLoading } from "../../redux/selects";
+import { useEffect } from "react";
+import { fetchAllContacts } from "../../redux/contactsOps";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllContacts());
+  }, [dispatch]);
+
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
+
   return (
     <>
       <div className="container">
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import { selectVisibleContacts } from "../../redux/selects";
 import s from "./ContactList.module.css";
-import { useEffect } from "react";
-import { fetchAllContacts } from "../../redux/contactsOps";
 
 const ContactList = () => {
   const visibleContacts = useSelector(selectVisibleContacts);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchAllContacts());
-  }, [dispatch]);
 
   return (
     <ul className={s.list}>
